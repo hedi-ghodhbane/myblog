33eb33ccba5d612697f1f06b5749e0846a556efb
hedi-ghodhbane <hedighodhbane@yahoo.com>
2025-05-06 14:12:57 +0100

    update home

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import solidJs from "@astrojs/solid-js"
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://luxury-starship-ba0578.netlify.app",
+  site: "https://hedighodhbane.com",
   integrations: [mdx(), sitemap(), solidJs(), tailwind({ applyBaseStyles: false })],
 })
